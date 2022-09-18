b8906a3c3dc47e911ec7b972b7876b22ea8ebc8b
glavdimir50 <didialive302@gmail.com>
2022-09-19 00:30:24 +0800

    Create GenericRepository

---
+++ NorthWindTest.DataAccess/Classes/GenericRepository.cs
@@ -0,0 +1,73 @@
+﻿using NorthWindTest.DataAccess.Interfaces;
+using NorthWindTest.Entity.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NorthWindTest.DataAccess.Classes
+{
+    public class GenericRepository<TEntity> : IGenericRepository<TEntity>, IDisposable
+        where TEntity : class
+    {
+        private readonly NorthwindContext _context;
+        public GenericRepository(NorthwindContext context) => _context = context;
+
+        /// <summary>
+        /// 新增
+        /// </summary>
+        /// <param name="entity"></param>
+        public void Create(TEntity entity)
+        {
+            _context.Set<TEntity>().Add(entity);
+            _context.SaveChanges();
+        }
+
+        /// <summary>
+        /// 修改
+        /// </summary>
+        /// <param name="entity"></param>
+        public void Update(TEntity entity)
+        {
+            _context.Update(entity);
+            _context.SaveChanges();
+        }
+
+        /// <summary>
+        /// 刪除
+        /// </summary>
+        /// <param name="entity"></param>
+        public void Delete(TEntity entity)
+        {
+            _context.Set<TEntity>().Remove(entity);
+            _context.SaveChanges();
+        }
+
+        /// <summary>
+        /// 全部查詢
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerable<TEntity> Filter()
+        {
+            return _context.Set<TEntity>();
+        }
+
+        /// <summary>
+        /// 條件查詢
+        /// </summary>
+        /// <param name="predicate"></param>
+        /// <returns></returns>
+        public IEnumerable<TEntity> Filter(Func<TEntity, bool> predicate)
+        {
+            return _context.Set<TEntity>().Where(predicate);
+        }
+
+        public void Dispose()
+        {
+            if (_context != null)
+            {
+                _context.Dispose();
+            }
+        }
+    }
+}
---
+++ NorthWindTest.DataAccess/Interfaces/IGenericRepository.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NorthWindTest.DataAccess.Interfaces
+{
+    public interface IGenericRepository<TEntity>
+    {
+        /// <summary>
+        /// 新增
+        /// </summary>
+        /// <param name="entity"></param>
+        void Create(TEntity entity);
+
+        /// <summary>
+        /// 修改
+        /// </summary>
+        /// <param name="predicate"></param>
+        /// <param name="entity"></param>
+        void Update(TEntity entity);
+
+        /// <summary>
+        /// 刪除
+        /// </summary>
+        /// <param name="entity"></param>
+        void Delete(TEntity entity);
+
+        /// <summary>
+        /// 全部查詢
+        /// </summary>
+        /// <returns></returns>
+        IEnumerable<TEntity> Filter();
+
+        /// <summary>
+        /// 條件查詢
+        /// </summary>
+        /// <param name="predicate"></param>
+        /// <returns></returns>
+        IEnumerable<TEntity> Filter(Func<TEntity, bool> predicate);
+    }
+}
