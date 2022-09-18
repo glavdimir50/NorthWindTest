1d14d270884405dced7a12d44ef8885c4848d1cb
glavdimir50 <didialive302@gmail.com>
2022-09-19 00:47:18 +0800

    Create CustomerService

---
+++ NorthWindTest.DataAccess/Classes/CustomerService.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using NorthWindTest.Business.MainBusiness.Interfaces;
+using NorthWindTest.DataAccess.Interfaces;
+using NorthWindTest.Entity.Entity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NorthWindTest.Business.MainBusiness.Classes
+{
+    public class CustomerService : ICustomerService
+    {
+        private IGenericRepository<Customers> _customersRepo;
+
+        public CustomerService(IServiceProvider provider)
+        {
+            _customersRepo = provider.GetRequiredService<IGenericRepository<Customers>>();
+        }
+
+        /// <summary>
+        /// 搜尋全部客戶
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerable<Customers> GetCustomers()
+        {
+            return _customersRepo.Filter();
+        }
+
+        /// <summary>
+        /// 根據客戶ID搜尋
+        /// </summary>
+        /// <param name="Id"></param>
+        /// <returns></returns>
+        public IEnumerable<Customers> GetCustomersById(string Id)
+        {
+            return _customersRepo.Filter(x=>x.CustomerId == Id);
+        }
+
+        /// <summary>
+        /// 根據客戶City搜尋
+        /// </summary>
+        /// <param name="City"></param>
+        /// <returns></returns>
+        public IEnumerable<Customers> GetCustomersByCity(string City)
+        {
+            return _customersRepo.Filter(x=>x.City == City);
+        }
+    }
+}
---
+++ NorthWindTest.DataAccess/Interfaces/ICustomerService.cs
@@ -0,0 +1,31 @@
+﻿using NorthWindTest.Entity.Entity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NorthWindTest.Business.MainBusiness.Interfaces
+{
+    public interface ICustomerService
+    {
+
+        /// <summary>
+        /// 搜尋全部客戶
+        /// </summary>
+        /// <returns></returns>
+        IEnumerable<Customers> GetCustomers();
+
+        /// <summary>
+        /// 根據客戶ID搜尋
+        /// </summary>
+        /// <param name="Id"></param>
+        /// <returns></returns>
+        IEnumerable<Customers> GetCustomersById(string Id);
+
+        /// <summary>
+        /// 根據客戶City搜尋
+        /// </summary>
+        /// <param name="City"></param>
+        /// <returns></returns>
+        IEnumerable<Customers> GetCustomersByCity(string City);
+    }
+}
