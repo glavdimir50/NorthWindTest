fcbe5183ce7e4fbce3331fb8c1eaf164b3c9a751
glavdimir50 <didialive302@gmail.com>
2022-09-19 23:06:09 +0800

    Add HomeController ICustomerService

--- NorthWindTest/Controllers/HomeController.cs
+++ NorthWindTest/Controllers/HomeController.cs
@@ -1,9 +1,17 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using NorthWindTest.Business.MainBusiness.Interfaces;
+using System;
 
 namespace NorthWindTest.Web.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ICustomerService _customerService;
+        public HomeController(IServiceProvider provider)
+        {
+            _customerService = provider.GetRequiredService<ICustomerService>();
+        }
         public IActionResult Index()
         {
             return View();
