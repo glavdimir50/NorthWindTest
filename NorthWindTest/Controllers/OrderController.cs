2c48a79c45ce0cfc9214ec93c58b996360dfed03
glavdimir50 <didialive302@gmail.com>
2022-09-20 19:37:50 +0800

    Create OrderQuery Page

---
+++ NorthWindTest/Controllers/OrderController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NorthWindTest.Web.Controllers
+{
+    public class OrderController : Controller
+    {
+        public IActionResult OrderQuery()
+        {
+            return View();
+        }
+    }
+}
